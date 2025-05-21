a730c0af8c6e07f3cadaa74162c01ca673c15a9a
Senelor K. Sebuava <123073561+SSenelor@users.noreply.github.com>
2025-05-21 20:38:47 +0000

    create hours_to_seconds.py

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#Declare all variables
+hours = 2
+seconds_in_one_hour = 3600
+
+#Calculations 
+seconds = seconds_in_one_hour * hours
+
+#Printing of results
+print(hours, "hours(s) is", seconds, "seconds")
