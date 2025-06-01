801e807af1bf7bc566936988672666cf10a03638
Senelor K. Sebuava <123073561+SSenelor@users.noreply.github.com>
2025-06-01 18:15:07 +0000

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,8 +2,6 @@ task = str(input("Input a task description: "))
 priority = str(input("What is the priority (high, medium, low): ")).lower()
 time_bound = str(input("Task is time-bound (yes or no): ")).lower()
 
-#Use a Match Case statement to react differently based on the task’s priority.
-#Within the Match Case or after, use an if statement to modify the reminder if the task is time-bound.
 print("Reminder: ", end = "")
 match priority :
     case "high":
@@ -16,8 +14,8 @@ match priority :
         print (f"{task}' has an unknown priority. Please double-check your input.")
 
 if time_bound == "yes":
-   # message1 = " that requires immediate attention today!"
+
     print(f"that requires immediate attention today!")
 else:
-  # message2 = ". Consider completing it when you have free time."
+
     print(f". Consider completing it when you have free time.")
